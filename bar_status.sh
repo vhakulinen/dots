e9e572932e0dd74e95fba74e251e4768cf68ce53
Ville Hakulinen <ville.hakulinen@gmail.com>
2015-11-09 14:46:07 +0200

    Lets user bar again!

--- bar_status.sh
+++ bar_status.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/bash
 
-`xset +fp /usr/share/fonts/local`
-`xset fp rehash`
 # Define the clock
 Clock() {
-        DATE=$(date "+%a %b %d, %T")
+    DATE=$(date "+%a %b %d, %T")
 
-        echo -n "$DATE"
+    echo -n "$DATE"
+}
+
+Music() {
+    STATUS=$(mpc status |grep -Po "[[a-z]*]")
+    CURRENT=$(mpc current)
+
+    echo -n "$STATUS $CURRENT"
+}
+
+Volume() {
+    VOL=$(amixer get Master |grep -Po "[0-9]*%")
+
+    echo -n "$VOL"
 }
 
 # Print the clock
 
 while true; do
-        echo -e "%{c}\u00B8 $(Clock)%{F-}"
-        sleep 1;
+    echo -e "   %{F#55aaff}$(Music)%{F#fff} %{c}$(Clock)%{F#00FFFF} %{r}$(Volume)   "
+    sleep 1;
 done
