0ecc0321d35ec9cf06a557aac910ef3a42daddd4
Ville Hakulinen <ville.hakulinen@gmail.com>
2024-05-28 23:43:17 +0300

    nvim: revert back to auto-pairs
    nvim-autopairs lacks at least one feature, which is to delete a pair
    that has unspecified amount if space inbetween the pair.

--- nvim/init.lua
+++ nvim/init.lua
@@ -42,13 +42,16 @@ local plugins = {
   -- Nice starting page.
   'mhinz/vim-startify',
 
-  { -- Progress messages
+  -- Automatic pairs.
+  'jiangmiao/auto-pairs',
+
+  { -- Progress messages.
     'j-hui/fidget.nvim',
     tag = 'v1.4.1', -- NOTE(ville): No stable branch available
     opts = { },
   },
 
-  { -- Treesitter
+  { -- Treesitter.
     'nvim-treesitter/nvim-treesitter',
     opts = { }
   },
@@ -78,69 +81,6 @@ local plugins = {
         position = 'top'
       }
     }
-  },
-
-  { -- Automatic pairs.
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    config = function()
-      local npairs = require'nvim-autopairs'
-      local Rule = require'nvim-autopairs.rule'
-      local cond = require'nvim-autopairs.conds'
-
-      npairs.setup {
-        enable_check_bracket_line = false
-      }
-
-
-      -- Insert '(' after selecting function or method item.
-      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-      local cmp = require 'cmp'
-      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-
-      -- Add spaces, i.e. space in "(|)" results to "( | )".
-      -- Sourced from nvim-autopairs wiki.
-      local brackets = { { '(', ')' }, { '[', ']' }, { '{', '}' } }
-      npairs.add_rules {
-        -- Rule for a pair with left-side ' ' and right side ' '
-        Rule(' ', ' ')
-          -- Pair will only occur if the conditional function returns true
-          :with_pair(function(opts)
-            -- We are checking if we are inserting a space in (), [], or {}
-            local pair = opts.line:sub(opts.col - 1, opts.col)
-            return vim.tbl_contains({
-              brackets[1][1] .. brackets[1][2],
-              brackets[2][1] .. brackets[2][2],
-              brackets[3][1] .. brackets[3][2]
-            }, pair)
-          end)
-          :with_move(cond.none())
-          :with_cr(cond.none())
-          -- We only want to delete the pair of spaces when the cursor is as such: ( | )
-          :with_del(function(opts)
-            local col = vim.api.nvim_win_get_cursor(0)[2]
-            local context = opts.line:sub(col - 1, col + 2)
-            return vim.tbl_contains({
-              brackets[1][1] .. '  ' .. brackets[1][2],
-              brackets[2][1] .. '  ' .. brackets[2][2],
-              brackets[3][1] .. '  ' .. brackets[3][2]
-            }, context)
-        end)
-      }
-      -- For each pair of brackets we will add another rule
-      for _, bracket in pairs(brackets) do
-        npairs.add_rules {
-          -- Each of these rules is for a pair with left-side '( ' and right-side ' )' for each bracket type
-          Rule(bracket[1] .. ' ', ' ' .. bracket[2])
-            :with_pair(cond.none())
-            :with_move(function(opts) return opts.char == bracket[2] end)
-            :with_del(cond.none())
-            :use_key(bracket[2])
-            -- Removes the trailing whitespace that can occur without this
-            :replace_map_cr(function(_) return '<C-c>2xi<CR><C-c>O' end)
-        }
-      end
-    end
   },
 
   { -- Telescope
